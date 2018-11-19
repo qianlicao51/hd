8963ca50f7cdff83d1388d8ac8ce6cf06fffbb04
zhuzi51 <qianlicaodemo@163.com>
2018-11-19 23:20:23 +0800

    日志RDD持久化

--- src/main/java/cn/zhuzi/sparksp01/LogSouGou.java
+++ src/main/java/cn/zhuzi/sparksp01/LogSouGou.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import org.apache.ibatis.io.Resources;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.storage.StorageLevel;
 
 import cn.zhuzi.spark.SparkUtils;
 
@@ -14,9 +15,12 @@ public class LogSouGou {
 
 		File logFile = Resources.getResourceAsFile("txt/SogouQ2.txt");
 		JavaSparkContext context = SparkUtils.getContext();
-
 		JavaRDD<String> javaRDD = context.textFile(logFile.getAbsolutePath());
+		javaRDD.persist(new StorageLevel(true, true, false, true, 1));
 
 		System.out.println(javaRDD.count());
+
+		long count = javaRDD.filter(t -> t.contains("131")).count();
+		System.out.println(count);
 	}
 }
