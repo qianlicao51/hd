3cd83866c946e09d9b199becef10238ec6d087ad
zhuzi51 <qianlicaodemo@163.com>
2018-11-23 14:48:31 +0800

    sparkStreaming一个简单例子

--- src/main/java/cn/zhuzi/spark/chart10/StreamDemo.java
+++ src/main/java/cn/zhuzi/spark/chart10/StreamDemo.java
@@ -1,7 +1,10 @@
 package cn.zhuzi.spark.chart10;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.function.Function;
 import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 
 /**
@@ -13,10 +16,52 @@ import org.apache.spark.streaming.api.java.JavaStreamingContext;
  *
  */
 public class StreamDemo {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
+
+		errorDemo(createStreamingContext());
+
+	}
+
+	/**
+	 * 创建 JavaStreamingContext
+	 * 
+	 * @returnJavaStreamingContext
+	 */
+	private static JavaStreamingContext createStreamingContext() {
 		// Create the context with a 5 second batch size
-		SparkConf sparkConf = new SparkConf().setAppName("sparkStreaming_Demo").setMaster("local");
-		JavaStreamingContext ssc = new JavaStreamingContext(sparkConf, Durations.seconds(5));
-		System.out.println(ssc);
+		// TODO 此处 创建一个拥有两个线程的应用程序 local[2] 一定是2个否则看不到效果
+		SparkConf sparkConf = new SparkConf().setAppName("sparkStreaming_Demo").setMaster("local[2]");
+		return new JavaStreamingContext(sparkConf, Durations.seconds(5));
+	}
+
+	/**
+	 * 打印包含error的行 <br>
+	 * 需要注意的是配置一个拥有两个线程的应用程序 local[2]<br>
+	 * nc 模拟数据 先启动发送端，<br>
+	 * 
+	 * nc 在windows 发送端: nc -l -p 端口 <br>
+	 * nc 在windows 接收端: nc 127.0.0.1 端口 <br>
+	 * 
+	 * @param ssc
+	 */
+	private static void errorDemo(JavaStreamingContext ssc) throws InterruptedException {
+		// 以8888 端口作为输入创建DStream
+		JavaReceiverInputDStream<String> socketTextStream = ssc.socketTextStream("127.0.0.1", 8888);
+		// 从中筛选出包含error的行
+
+		// JavaDStream<String> javaDStream =
+		// socketTextStream.filter(t->t.contains("error"));
+		// 打印有error的行
+		JavaDStream<String> dStream = socketTextStream.filter(new Function<String, Boolean>() {
+			@Override
+			public Boolean call(String v1) throws Exception {
+				return v1.contains("error");
+			}
+		});
+		dStream.print();
+		// 启动流计算环境
+		ssc.start();
+		// 等待作业完成
+		ssc.awaitTermination();
 	}
 }
